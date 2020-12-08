f30f4a76108d7f0b3ad119397dec1f3c6e4915e4
jmpout <jmpout@riseup.net>
2020-12-08 16:19:46 +0100

    JWT config added.

--- backend/src/config.py
+++ backend/src/config.py
@@ -14,3 +14,10 @@ class BaseConfig:
         default="postgresql://postgres:postgres@db:5432/postgres"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Flask-JWT-Extended
+    JWT_ACCESS_TOKEN_EXPIRES = 300  # 5 minutes
+    JWT_REFRESH_TOKEN_EXPIRES = 43200  # 12 hours
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
+    JWT_SECRET_KEY = "mysecretkey"  # CHANGE THE KEY!
