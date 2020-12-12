21f9170b99165fcf005e6dcd23b3f8aec4cd8d98
jmpout <jmpout@riseup.net>
2020-12-13 00:11:51 +0100

    Restructuring of Dockerifle and app.

--- backend/src/app.py
+++ backend/src/app.py
@@ -14,13 +14,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object("src.config.BaseConfig")
     CORS(app)
-
     jwt.init_app(app)
-
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
-
     app.register_blueprint(api, url_prefix="/api")
 
     return app
